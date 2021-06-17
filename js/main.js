a12bedcecf3d4b95a1a853e898e1fd7d704c6f79
duiok <54449342+duiok@users.noreply.github.com>
2021-06-17 13:09:55 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 const slider = document.querySelector("#slider");
 let sliderSection = document.querySelectorAll(".slider_section");
 let sliderSectionLast = sliderSection[sliderSection.length - 1]; 
@@ -38,9 +37,9 @@ btnLeft.addEventListener('click', function(){
     prev();
 });
 
-/* setInterval(function(){
+setInterval(function(){
     next();
-}, 5000); */
+}, 5000);
 
 const menu = document.querySelector('.menu');
 const ipad = window.matchMedia('screen and (max-width: 767px)')
@@ -63,72 +62,5 @@ function hideShow(){
     }else{
         menu.classList.add('is-active');
     }
-
-=======
-const slider = document.querySelector("#slider");
-let sliderSection = document.querySelectorAll(".slider_section");
-let sliderSectionLast = sliderSection[sliderSection.length - 1]; 
-
-const btnLeft = document.querySelector("#btn-left");
-const btnRigth = document.querySelector("#btn-right");
-
-slider.insertAdjacentElement('afterbegin', sliderSectionLast);
-
-function next(){
-    let sliderSectionFirst = document.querySelectorAll(".slider_section")[0];
-     slider.style.marginLeft = "-200%";
-     slider.style.transition = "all 0.5s";
-     setTimeout(function(){
-         slider.style.transition = "none";
-         slider.insertAdjacentElement('beforeend', sliderSectionFirst);
-         slider.style.marginLeft = "-100%";
-     }, 500);
-}
-function prev(){
-    let sliderSection = document.querySelectorAll(".slider_section");
-    let sliderSectionLast = sliderSection[sliderSection.length - 1];
-     slider.style.marginLeft = "0";
-     slider.style.transition = "all 0.5s";
-     setTimeout(function(){
-         slider.style.transition = "none";
-         slider.insertAdjacentElement('afterbegin', sliderSectionLast);
-         slider.style.marginLeft = "-100%";
-     }, 500);
-}
-
-btnRigth.addEventListener('click', function(){
-    next();
-});
-
-btnLeft.addEventListener('click', function(){
-    prev();
-});
-
-/* setInterval(function(){
-    next();
-}, 5000); */
-
-const menu = document.querySelector('.menu');
-const ipad = window.matchMedia('screen and (max-width: 767px)')
-
-const burguerButton = document.querySelector('#burger-menu');
-
-function validation(event){
-    if(event.matches){
-        burguerButton.addEventListener('click', hideShow);
-    }else{
-        burguerButton.removeEventListener('click', hideShow);
-    }
-}
-
-validation(ipad);
-
-function hideShow(){
-    if(menu.classList.contains('is-active')){
-        menu.classList.remove('is-active');
-    }else{
-        menu.classList.add('is-active');
-    }
-
 
 }
